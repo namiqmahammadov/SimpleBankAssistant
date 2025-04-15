2acf6c8d91cb03156cf36ddb8584703cbd123127
namiqmahammadov <130308957+namiqmahammadov@users.noreply.github.com>
2025-04-14 22:02:19 -0400

    Initial commit

---
+++ src/main/java/az/risk/SimpleBankAssistant/SimpleBankAssistantApplication.java
@@ -0,0 +1,13 @@
+package az.risk.SimpleBankAssistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBankAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBankAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/az/risk/SimpleBankAssistant/SimpleBankAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package az.risk.SimpleBankAssistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBankAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
